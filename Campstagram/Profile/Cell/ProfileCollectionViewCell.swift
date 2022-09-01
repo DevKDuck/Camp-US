14ac666926f3c6b6e7d22f1a2ad88299c12da8f6
DevKDuck <loading95@naver.com>
2022-09-01 23:59:48 +0900

    프로필창 추가

--- Campstagram/Profile/Cell/ProfileCollectionViewCell.swift
+++ Campstagram/Profile/Cell/ProfileCollectionViewCell.swift
@@ -10,6 +10,11 @@ import UIKit
 class ProfileCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var addProfileImageView: UIImageView!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var addFriendsButton: UIButton!
+    @IBOutlet weak var followerCountLabel: UILabel!
+    @IBOutlet weak var followingCountLabel: UILabel!
+    @IBOutlet weak var postingCountLabel: UILabel!
     static let identifier = "ProfileCollectionViewCell" //collectionView가 생성할 cell임을 알려주자
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,7 +24,16 @@ class ProfileCollectionViewCell: UICollectionViewCell {
 
     private func setAttribute() {
         profileImageView.cornerRadius = 88 / 2
-        addProfileImageView.cornerRadius = 24 / 2 
+        addProfileImageView.cornerRadius = 24 / 2
+        editButton.layer.cornerRadius = 5
+        editButton.layer.borderColor = UIColor.lightGray.cgColor
+        editButton.layer.borderWidth = 1
         
+//        addFriendsButton.layer.cornerRadius = 3
+//        addFriendsButton.layer.borderColor = UIColor.lightGray.cgColor
+//        addFriendsButton.layer.borderWidth = 1
+        
+        [postingCountLabel, followerCountLabel, followingCountLabel]
+            .forEach{ $0.text = "\(Int.random(in: 0...10))"}
     }
 }
